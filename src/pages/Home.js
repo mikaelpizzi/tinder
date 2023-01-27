6498938aefc8e480ddb965847a8de1671aebb8f2
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-26 20:18:13 -0400

    feat: Implement home page structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Nav from "../components/Nav";
 
 const Home = () => {
-  return <div>Home</div>;
+  const authToken = true;
+
+  const handleClick = () => {
+    console.log("clicked!");
+  };
+
+  return (
+    <>
+      <Nav />
+      <div className="home">
+        <h1>Swipe Right&#174;</h1>
+        <button className="primary-button" onClick={handleClick}>
+          {authToken ? "Sign out" : "Create Account"}
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Home;
