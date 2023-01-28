364b01a7e82afa0de82e78c2ad1e8c9cbc8f7ee2
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-28 19:23:26 -0400

    feat: Implement matches display component

---
+++ src/components/MatchesDisplay.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Chat from "./Chat";
+import ChatInput from "./ChatInput";
+
+const MatchesDisplay = () => {
+  return (
+    <>
+      <Chat />
+      <ChatInput />
+    </>
+  );
+};
+
+export default MatchesDisplay;
