ab1e31944f55c02f71af5c85e896651f655385d1
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-28 23:53:21 -0400

    feat: get users from mongodb

--- server/index.js
+++ server/index.js
@@ -1,1 +1,31 @@
-console.log("running");
+const PORT = 8000;
+
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://mozart:amadeus@cluster0.tgjmuwg.mongodb.net/?retryWrites=true&w=majority";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
+
+app.post("/signup", (req, res) => {
+  res.json("Hello");
+});
+
+app.get("/users", async (req, res) => {
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    const database = client.db("app-data");
+    const users = database.collection("users");
+
+    const returnedUsers = await users.find().toArray();
+    res.send(returnedUsers);
+  } finally {
+    await client.close();
+  }
+});
+
+app.listen(PORT, () => console.log("Server running on port ", PORT));
