2d49d08363d679a7732559cbfa0691d31155194b
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-31 22:03:55 -0400

    fix: Fix auth modal log in

--- client/src/components/AuthModal.js
+++ client/src/components/AuthModal.js
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import axios from "axios";
-import { useCookie } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const AuthModal = ({ setShowModal, isSignUp }) => {
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState(null);
   const [error, setError] = useState(null);
-  const navigate = useNavigate();
-  const [cookies, setCookie, removeCookie] = useCookie(["user"]);
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+
+  let navigate = useNavigate();
 
   console.log(email, password, confirmPassword);
 
@@ -22,26 +23,23 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
 
     try {
       if (isSignUp && password !== confirmPassword) {
-        setError("Passwords do not match!");
+        setError("Passwords need to match!");
         return;
       }
 
       const response = await axios.post(
         `http://localhost:8000/${isSignUp ? "signup" : "login"}`,
-        {
-          email,
-          password,
-        }
+        { email, password }
       );
 
-      setCookie("Email", response.data.email);
-      setCookie("UserId", response.data.userId);
       setCookie("AuthToken", response.data.token);
+      setCookie("UserId", response.data.userId);
 
       const success = response.status === 201;
-
       if (success && isSignUp) navigate("/onboarding");
       if (success && !isSignUp) navigate("/dashboard");
+
+      window.location.reload();
     } catch (error) {
       console.log(error);
     }
@@ -49,12 +47,13 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
 
   return (
     <div className="auth-modal">
-      <span onClick={handleClick} className="close-icon">
-        &#10006; <br />
-      </span>
+      <div className="close-icon" onClick={handleClick}>
+        ⓧ
+      </div>
+
       <h2>{isSignUp ? "CREATE ACCOUNT" : "LOG IN"}</h2>
       <p>
-        By clicking "Log in", you agree to our terms. Learn how we process your
+        By clicking Log In, you agree to our terms. Learn how we process your
         data in our Privacy Policy and Cookie Policy.
       </p>
       <form onSubmit={handleSubmit}>
@@ -66,7 +65,6 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
           required={true}
           onChange={(e) => setEmail(e.target.value)}
         />
-
         <input
           type="password"
           id="password"
@@ -75,7 +73,6 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
           required={true}
           onChange={(e) => setPassword(e.target.value)}
         />
-
         {isSignUp && (
           <input
             type="password"
@@ -86,9 +83,8 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
         )}
-
-        <input type="submit" className="secondary-button" />
-        <p>{error && error}</p>
+        <input className="secondary-button" type="submit" />
+        <p>{error}</p>
       </form>
 
       <hr />
@@ -96,5 +92,4 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
     </div>
   );
 };
-
 export default AuthModal;
