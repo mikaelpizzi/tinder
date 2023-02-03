52feb663e7ab7f4a39c2a97a7ddb3a9a7be1a48d
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-02-03 01:23:55 -0400

    feat: Only show home if user is not logged

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -63,32 +63,36 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="dashboard">
-      <ChatContainer user={user} />
-      <div className="swipe-container">
-        <div className="card-container">
-          {characters.map((character) => (
-            <TinderCard
-              className="swipe"
-              key={character.name}
-              onSwipe={(dir) => swiped(dir, character.name)}
-              onCardLeftScreen={() => outOfFrame(character.name)}
-            >
-              <div
-                style={{ backgroundImage: "url(" + character.url + ")" }}
-                className="card"
-              >
-                <h3>{character.name}</h3>
-              </div>
-            </TinderCard>
-          ))}
+    <>
+      {user && (
+        <div className="dashboard">
+          <ChatContainer user={user} />
+          <div className="swipe-container">
+            <div className="card-container">
+              {characters.map((character) => (
+                <TinderCard
+                  className="swipe"
+                  key={character.name}
+                  onSwipe={(dir) => swiped(dir, character.name)}
+                  onCardLeftScreen={() => outOfFrame(character.name)}
+                >
+                  <div
+                    style={{ backgroundImage: "url(" + character.url + ")" }}
+                    className="card"
+                  >
+                    <h3>{character.name}</h3>
+                  </div>
+                </TinderCard>
+              ))}
 
-          <div className="swipe-info">
-            {lastDirection ? <p>You swiped {lastDirection}</p> : <p />}
+              <div className="swipe-info">
+                {lastDirection ? <p>You swiped {lastDirection}</p> : <p />}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
