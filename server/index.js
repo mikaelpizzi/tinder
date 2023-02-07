995827a0aae655da01a1d5bed7e68771e2ec149e
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-02-07 01:15:25 -0400

    feat: Display messages

--- server/index.js
+++ server/index.js
@@ -207,4 +207,42 @@ app.put("/user", async (req, res) => {
   }
 });
 
+// Get Messages by from_userId and to_userId
+app.get("/messages", async (req, res) => {
+  const { userId, correspondingUserId } = req.query;
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const database = client.db("app-data");
+    const messages = database.collection("messages");
+
+    const query = {
+      from_userId: userId,
+      to_userId: correspondingUserId,
+    };
+    const foundMessages = await messages.find(query).toArray();
+    res.send(foundMessages);
+  } finally {
+    await client.close();
+  }
+});
+
+// Add a Message to our Database
+app.post("/message", async (req, res) => {
+  const client = new MongoClient(uri);
+  const message = req.body.message;
+
+  try {
+    await client.connect();
+    const database = client.db("app-data");
+    const messages = database.collection("messages");
+
+    const insertedMessage = await messages.insertOne(message);
+    res.send(insertedMessage);
+  } finally {
+    await client.close();
+  }
+});
+
 app.listen(PORT, () => console.log("server running on PORT " + PORT));
